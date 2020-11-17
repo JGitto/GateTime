6e3ff64f431f5ac391944412f7a1486f394dc68c
JGitto <jakob@lenkenet.de>
2020-11-17 14:08:18 +0100

    Added chip.java class

---
+++ src/Chip.java
@@ -0,0 +1,11 @@
+public class Chip extends Gate{
+
+    public Chip(String name, int inputSize, int outputSize) {
+        super(name, inputSize, outputSize);
+    }
+
+    @Override
+    public void compute() {
+
+    }
+}
